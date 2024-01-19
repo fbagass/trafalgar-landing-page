366d92d17e2757d59c090acdae641f7afa274a06
fbagass <fajarbagassetiawan01@gmail.com>
2024-01-19 08:44:48 +0700

    bg element header

--- src/App.js
+++ src/App.js
@@ -30,27 +30,36 @@ function App() {
         </div>
       </div>
       {/* Header */}
-      <div className="header grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-5">
-        <div className="header-1 col-span-2">
-          <img
-            src={process.env.PUBLIC_URL + "/Assets/element.png"}
-            alt="element"
-          />
-          <h1>Virtual healthcare for you</h1>
-          <p>
-            Trafalgar provides progressive, and affordable healthcare,
-            accessible on mobile and online for everyone
-          </p>
-          <div className="consult-btn text-center">Consult today</div>
-        </div>
-        <div className="header-2 col-span-3 flex justify-end">
-          <img
-            src={
-              process.env.PUBLIC_URL +
-              "/Assets/trafalgar-header-illustration-1.png"
-            }
-            alt="header-illustration"
-          />
+      <div
+        className="header-bg w-100"
+        style={{
+          backgroundImage: `url(${
+            process.env.PUBLIC_URL + "/Assets/element.png"
+          })`,
+        }}
+      >
+        <div className="header container grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-5">
+          <div className="header-1 col-span-2">
+            {/* <img
+              src={process.env.PUBLIC_URL + "/Assets/element.png"}
+              alt="element"
+            /> */}
+            <h1>Virtual healthcare for you</h1>
+            <p>
+              Trafalgar provides progressive, and affordable healthcare,
+              accessible on mobile and online for everyone
+            </p>
+            <div className="consult-btn text-center">Consult today</div>
+          </div>
+          <div className="header-2 col-span-3 flex justify-end">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/Assets/trafalgar-header-illustration-1.png"
+              }
+              alt="header-illustration"
+            />
+          </div>
         </div>
       </div>
     </div>
